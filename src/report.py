11e6b5eab46ec5fb4c29276daf46906512264ea9
Stijn Goossens <22433228+StijnGoossens@users.noreply.github.com>
2022-12-21 19:43:18 +0100

    feat: add script to create plots for specific period

---
+++ src/report.py
@@ -0,0 +1,32 @@
+"""Create timeseries plots for a specified time interval."""
+
+import sqlite3
+import pandas as pd
+from config import DB_PATH
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+
+from_date = "27/11/2022"
+# to_date = "4/12/2022"
+to_date = None
+
+from_date = datetime.strptime(from_date, "%d/%m/%Y")
+to_date = datetime.strptime(from_date, "%d/%m/%Y") if to_date else datetime.now()
+
+print(from_date, to_date)
+# delta_days = (to_date - from_date + 1) * 60 * 24
+
+con = sqlite3.connect(DB_PATH, detect_types=sqlite3.PARSE_DECLTYPES)
+df = pd.read_sql_query(f'SELECT *, date as "[timestamp]" FROM records WHERE date > "{from_date}" ORDER BY date DESC', con)
+# # Transform the date to a localised datetime.
+df['date'] = pd.to_datetime(df['date'])
+df["date"] = df["date"].dt.tz_localize("Europe/Brussels")
+
+def plot_metric(df: pd.DataFrame, metric: str) -> None:
+    plt.figure(figsize=(20,5))
+    plt.plot(df["date"], df[metric])
+    plt.ylabel(metric)
+    plt.savefig(f"/home/pi/Documents/rpi-airquality/output/{metric}_over_time.jpg")
+
+plot_metric(df, "co2")
